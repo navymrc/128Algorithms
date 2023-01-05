c43ac459ba25e478aac5cd3b2999c8721f75ca94
navy+mrc <navymrc@gmail.com>
2023-01-05 11:47:38 -0500

    Some Techniques: three partition added

--- Techniques/Encodind of sets by integers/technique.py
+++ Techniques/Encodind of sets by integers/technique.py
@@ -11,11 +11,32 @@ def min_(sets):
     """
     elem = 0
     for item in sets:
-        elem += 2 ** item
+        elem += 1 << item
 
     return int(math.log(-elem & elem)/math.log(2))
 
 
-r = min_((103, 91, 14))
+# minimum example
+#r = min_((103, 91, 14))
+#print(f"{r}\n{[0]* (1<<2)}")
 
-print(f"{r}\n{[0]* (1<<2)}")
+
+def three_partition(x):
+    """
+
+    :param x:
+    :return:
+    """
+    f = [0] * (1 << len(x))
+    for i, _ in enumerate(x):
+        for S in range(1 << i):
+            f[S | (1 << i)] = f[S] + x[i]
+    for A in range(1 << len(x)):
+        for B in range(1 << len(x)):
+            if A & B == 0 and f[A] == f[B] and 3 * f[A] == f[-1]:
+                return A, B, ((1 << len(x)) - 1) ^ A ^ B
+    return None
+
+
+x = three_partition([2, 2, 2, 2, 2, 2])
+print(f"{x}")
